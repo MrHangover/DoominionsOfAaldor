28f4ad6c78eb805bdd482876e01fe9281c4adf8d
Patchi92 <lechieu@hotmail.com>
2014-03-08 22:56:25 +0100

    Intro - AI - Fixes

---
+++ Assets/Scripts/EnemyArea.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyArea : MonoBehaviour {
+
+	public Transform Player;
+	public Transform Enemy;
+	
+	float Movement = 8f;
+
+	bool Combat = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+
+			if(Combat == false)
+			{
+				Enemy.position = Vector3.MoveTowards(transform.position, Player.position, Movement*Time.deltaTime);
+				
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			Combat = true;
+			
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			Combat = false;
+			
+		}
+	}
+
+}
