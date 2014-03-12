6fb4c750f2dff1ce8f16654d1cf2e058e7103b75
Patchi92 <lechieu@hotmail.com>
2014-03-12 17:32:56 +0100

    Player AI
    Trying to make it work.. Giving up for today :o

---
+++ Assets/Scripts/AIPlayer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIPlayer : MonoBehaviour {
+
+	public Transform Player;
+	public Transform PlayerAI;
+	
+	float Movement = 10f;
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		PlayerAI.position = Vector3.MoveTowards(transform.position, Player.position, Movement*Time.deltaTime);
+		//Movement = 10f;
+	}
+	
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+
+				Movement = 0f;
+				
+		}
+
+		if(other.gameObject.name == "AI-Player")
+		{
+			
+			Movement = 0f;
+			
+		}
+
+		if(other.gameObject.name == "AI-Player2")
+		{
+			
+			Movement = 0f;
+			
+		}
+	}
+	
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			Movement = 10f;
+			
+		}
+
+		if(other.gameObject.name == "AI-Player")
+		{
+			
+			Movement = 10f;
+			
+		}
+		
+		if(other.gameObject.name == "AI-Player2")
+		{
+			
+			Movement = 10f;
+			
+		}
+
+	}
+	
+}
