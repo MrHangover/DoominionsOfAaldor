abfa71827f4edc7844a0b13323426a1299f01e74
Patchi92 <lechieu@hotmail.com>
2014-04-18 23:28:36 +0200

    Martin wants me to sync

--- Assets/Scripts/Patchi/Patchi.cs
+++ Assets/Scripts/Patchi/Patchi.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Patchi : Player {
 	
+	Animator AnimBody;
 
 	float DefBallCD = 0;
 	float AttackCakeCD = 0;
@@ -28,12 +29,15 @@ public class Patchi : Player {
 	// Use this for initialization
 	void Start () {
 
+		AnimBody = GetComponent<Animator>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		AnimBody.SetFloat("moveV", Mathf.Abs(moveV));
+		AnimBody.SetFloat("moveH", Mathf.Abs(moveH));
 
 		if(SpeedTime < Time.time && UltTime < Time.time )
 		{
