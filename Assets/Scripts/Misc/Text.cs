c2392dbe44ee32308078fd20a82fdb9eb1008db6
Patchi92 <lechieu@hotmail.com>
2014-04-26 22:52:45 +0200

    Speech Fixed - UI almost done

--- Assets/Scripts/Misc/Text.cs
+++ Assets/Scripts/Misc/Text.cs
@@ -5,6 +5,21 @@ public class Text : MonoBehaviour {
 
 	GameObject ActivePlayer;
 
+	bool Begin = false;
+
+	string stringToEdit = "Hello";
+
+	bool TextTwo = true;
+	bool TextTwoDone = false;
+
+	bool TextThree = false;
+	bool TextThreeDone = false;
+
+	bool TextFour = false;
+	bool TextFourDone = false;
+
+	float NextTimer = -1f;
+
 	// Use this for initialization
 	void Start () {
 
@@ -24,10 +39,106 @@ public class Text : MonoBehaviour {
 		if(other.gameObject.tag == "Player")
 		{
 			Talk.Speechset();
+			Begin = true;
+			NextTimer = 100f + Time.time;
+		}
 
+	}
+
+	void OnGUI() {
+		
+		if(Begin)
+		{
 
 		
+			stringToEdit = GUI.TextField(new Rect(580, 500, 800, 200), stringToEdit, 2000);
+
+
+
+			if(Input.GetButtonUp("Normal"))
+			{
+
+				if (NextTimer < Time.time)
+					Next ();
+
+
+
+				if(TextTwo)
+				{
+					stringToEdit = "page 1";
+					Done ();
+				}
+
+				if(TextThree)
+				{
+					stringToEdit = "page 2";
+					Done ();
+				}
+
+				if(TextFour)
+				{
+					stringToEdit = "page 3";
+					Done ();
+				}
+
+			}
+
+
+			
+			
+		
 		}
+		
+		
+	}
+
+	void Next(){
+
+
+		if(TextTwo && TextTwoDone)
+		{
+			TextThree = true;
+			TextTwo = false;
+		}
+
+		if(TextThree && TextThreeDone)
+		{
+			TextFour = true;
+			TextThree = false;
+		}
+
+		if(TextFour && TextFourDone)
+		{
+			TextFour = false;
+			Begin = false;
+		}
+			
+
+
+
+	}
+
+	void Done(){
+
+
+		if(TextTwo && !TextTwoDone)
+		{
+			NextTimer = 1f + Time.time;
+			TextTwoDone = true;
+		}
+
+		if(TextThree && !TextThreeDone)
+		{
+			NextTimer = 1f + Time.time;
+			TextThreeDone = true;
+		}
+
+		if(TextFour && !TextFourDone)
+		{
+			NextTimer = 1f + Time.time;
+			TextFourDone = true;
+		}
+
 	}
 
 }
---
+++ Assets/Scripts/Misc/UI.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+
+
+	GameObject ActivePlayer;
+	
+	string offensive = "Ready";
+	string movement = "Ready";
+	string defensive = "Ready";
+	string ult = "Ready";
+
+	public bool Begin = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		ActivePlayer = GameObject.FindWithTag("Player");
+		Player Timer = ActivePlayer.GetComponent<Player>();
+
+		Debug.Log(Timer.offensiveCDTimer);
+
+		if(Timer.offensiveCDTimer > 0f)
+		{
+			Debug.Log ("lololo");
+			string offensive = "Test";
+		} else {
+			string offensive = "Ready";
+		}
+
+		if(Timer.movementCDTimer > 0f)
+		{
+			float movement = Timer.movementCDTimer;
+		} else {
+			string movement = "Ready";
+		}
+
+		if(Timer.defensiveCDTimer > 0f)
+		{
+			float offensive = Timer.defensiveCDTimer;
+		} else {
+			string offensive = "Ready";
+		}
+
+		if(Timer.ultCDTimer > 0f)
+		{
+			float offensive = Timer.ultCDTimer;
+		} else {
+			string offensive = "Ready";
+		}
+
+	}
+
+	void OnGUI() {
+
+		if(Begin)
+			{
+
+				GUILayout.BeginArea(new Rect(800,750,400,Screen.width / 2));
+
+				GUILayout.BeginHorizontal();
+
+				GUILayout.BeginVertical();
+				GUILayout.Label("Offensive");
+				GUILayout.EndVertical();
+
+				GUILayout.BeginVertical();
+				GUILayout.Label("Movement");
+				GUILayout.EndVertical();
+
+				GUILayout.BeginVertical();
+				GUILayout.Label("Defensive");
+				GUILayout.EndVertical();
+
+				GUILayout.BeginVertical();
+				GUILayout.Label("Ultimate");
+				GUILayout.EndVertical();
+
+				GUILayout.EndHorizontal();
+
+
+				GUILayout.BeginHorizontal();
+				
+				GUILayout.BeginVertical();
+				GUI.Box(new Rect(0, 30, 50, 50), offensive);
+				GUILayout.EndVertical();
+				
+				GUILayout.BeginVertical();
+				GUI.Box(new Rect(100, 30, 50, 50), movement);
+				GUILayout.EndVertical();
+				
+				GUILayout.BeginVertical();
+				GUI.Box(new Rect(200, 30, 50, 50), defensive);
+				GUILayout.EndVertical();
+				
+				GUILayout.BeginVertical();
+				GUI.Box(new Rect(300, 30, 50, 50), ult);
+				GUILayout.EndVertical();
+				
+				GUILayout.EndHorizontal();
+
+			
+				
+
+				GUILayout.EndArea();
+		}
+
+
+	}
+}
